6daa0c99830a76aaac394ac3f08e919a3c82653f
Diaa <Diaa@192.168.1.11>
2022-07-26 23:01:41 +0200

    gjgyj

---
+++ multiplyFn.m
@@ -0,0 +1,10 @@
+out = app.Input.Value * app.PubNum + 1;
+
+app.displayResult(out)
+
+waveinfo
+
+x = "dfg";
+if x == "text"
+
+end
---
+++ multiplyFnWithApp.m
@@ -0,0 +1,7 @@
+function multiplyFnWithApp(app)
+
+out = app.Input.Value * app.PrvNum + 1;
+
+app.displayResult(out)
+
+end
